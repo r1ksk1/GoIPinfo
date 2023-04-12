fbeb29a6dfced5086ce592cb8bb04f62ed529179
r1ksk1 <54509000+r1ksk1@users.noreply.github.com>
2023-04-12 13:07:56 +0100

    Create main.go

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+)
+
+func main() {
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "IP address: %s\n", r.RemoteAddr)
+        fmt.Fprintf(w, "Hostname: %s\n", r.Host)
+        fmt.Fprintf(w, "User-Agent: %s\n", r.Header.Get("User-Agent"))
+        fmt.Fprintf(w, "Language: %s\n", r.Header.Get("Accept-Language"))
+    })
+
+    http.ListenAndServe(":8448", nil)
