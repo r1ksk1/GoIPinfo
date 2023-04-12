43ac5a79e675acd7bc3de64b64ae51700f1d9328
r1ksk1 <54509000+r1ksk1@users.noreply.github.com>
2023-04-12 15:00:24 +0100

    Update main.go
    upgraded version - looks up ip address for hostname.

--- main.go
+++ main.go
@@ -2,16 +2,36 @@ package main
 
 import (
     "fmt"
+    "net"
     "net/http"
 )
 
 func main() {
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "IP address: %s\n", r.RemoteAddr)
-        fmt.Fprintf(w, "Hostname: %s\n", r.Host)
-        fmt.Fprintf(w, "User-Agent: %s\n", r.Header.Get("User-Agent"))
-        fmt.Fprintf(w, "Language: %s\n", r.Header.Get("Accept-Language"))
+        ipAddr := r.RemoteAddr
+        hostname, _ := lookupHostname(ipAddr)
+        userAgent := r.Header.Get("User-Agent")
+        language := r.Header.Get("Accept-Language")
+
+        fmt.Fprintf(w, "Client IP address: %s\n", ipAddr)
+        fmt.Fprintf(w, "Client hostname: %s\n", hostname)
+        fmt.Fprintf(w, "Client user agent: %s\n", userAgent)
+        fmt.Fprintf(w, "Client language: %s\n", language)
     })
 
     http.ListenAndServe(":8448", nil)
+}
+
+func lookupHostname(ipAddr string) (string, error) {
+    addr, err := net.ResolveIPAddr("ip", ipAddr)
+    if err != nil {
+        return "", err
+    }
+
+    names, err := net.LookupAddr(addr.String())
+    if err != nil || len(names) == 0 {
+        return "", err
+    }
+
+    return names[0], nil
 }
